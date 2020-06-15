8ae6b357d6d122045e3c9e6b2c01a5c75f675ac9
Khamit <khamitamantaev@gmail.com>
2020-06-15 23:45:15 +0500

    Середина без сохранения в базу, завтра  буду разбираться в MySql и hibernate Query()

---
+++ vgrupper/src/main/java/com/vgrupper/demo/service/MyUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.vgrupper.demo.service;
+import com.vgrupper.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return new User("foo", "foo",
+                new ArrayList<>());
+    }
+}
